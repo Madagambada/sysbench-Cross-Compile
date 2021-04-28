8542b3fb336f4bdbdf3dc12204faa8b476763d1e
Madagambada <47931256+Madagambada@users.noreply.github.com>
2021-04-29 00:23:05 +0200

    Update armhf.sh

--- armhf.sh
+++ armhf.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+cat <<EOF >backport.list
+deb http://deb.debian.org/debian buster-backports main
+EOF
+mv backport.list /etc/apt/sources.list.d/
+
 dpkg --add-architecture armhf
 apt update
-apt -y install make crossbuild-essential-armhf git automake libtool pkg-config libaio-dev:armhf libluajit-5.1-dev:armhf -y
+apt -y install make crossbuild-essential-armhf git automake libtool pkg-config libaio-dev:armhf libck-dev:armhf libluajit-5.1-dev:armhf -y
 
 git clone https://github.com/akopytov/sysbench.git
 cd sysbench
 git checkout $(git tag | sort -V | tail -1)
 
 ./autogen.sh
-./configure CFLAGS="-static" LDFLAGS="-static" --host=arm-linux-gnueabihf --with-system-luajit --without-mysql
+./configure CFLAGS="-static" LDFLAGS="-static" --host=arm-linux-gnueabihf --with-system-luajit --with-system-ck --without-mysql
 make -j$(nproc)
 
 mv src/sysbench ../sysbench-linux-armhf
