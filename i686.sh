748f8eabcca65d2b1f9e3a301fb714604c3f5535
Madagambada <47931256+Madagambada@users.noreply.github.com>
2021-04-28 23:37:21 +0200

    Create i686.sh

---
+++ i686.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dpkg --add-architecture i386
+apt update
+apt -y install make crossbuild-essential-i386 git automake libtool pkg-config libaio-dev:i386 libmysqlclient-dev:i386 libssl-dev:i386 libpq-dev:i386 libluajit-5.1-dev:i386 -y
+
+git clone https://github.com/akopytov/sysbench.git
+cd sysbench
+git checkout $(git tag | sort -V | tail -1)
+
+./configure CFLAGS="-static -m32" LDFLAGS="-static" --host=i686-linux-gnu --with-system-luajit --without-mysql
+make -j$(nproc)
